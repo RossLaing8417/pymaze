688c0f8e75079ef6d65d04938c48337d9e609987
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2024-12-15 21:22:58 +0200

    ch 4

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from window import Window
+from point import Point
+from line import Line
 
 if __name__ == "__main__":
     win = Window(800, 600, "PyMaze")
+    win.draw_line(Line(Point(100, 100), Point(700, 500)), "black")
     win.wait_for_close()
